0924af1761f5461b57ed5877c025ae6fd17fa5ec
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-21 18:32:18 -0800

    Initial commit

--- src/stores/moviesStore.js
+++
@@ -1,8 +0,0 @@
-// src/stores/moviesStore.js
-import { writable } from 'svelte/store';
-
-export const movies = writable([
-    { id: 1, title: 'Transformer One', image: '/assets/images/movie1.jpg', rating: 0, votes: 0 },
-    { id: 2, title: 'John Wick', image: '/assets/images/movie2.jpg', rating: 0, votes: 0 },
-    { id: 3, title: 'Joker', image: '/assets/images/movie3.jpg', rating: 0, votes: 0 }
-]);
